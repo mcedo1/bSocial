8c870aedb17ef7b1c652645ea2a91fada7c1c9cd
Milos Sosic <milossosic516@gmail.com>
2024-02-10 13:33:56 +0100

    Update for docker compose

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,13 +1,13 @@
-const mysql = require("mysql");
-// const mysql2 = require("mysql2");
+// const mysql = require("mysql");
+const mysql2 = require("mysql2");
 
 //For Docker-compose testing use:mysql2 where host:mysql
-const connection = mysql.createConnection({
-    host: "localhost",
+const connection = mysql2.createConnection({
+    host: "mysql",
     user: "root",
     password: "root",
     database: "bdata",
-    port: "3307",
+    port: "3306",
 });
 
 connection.connect((err) => {
